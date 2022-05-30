38b440167031da5f2a86d765676e054d61db5b0b
Xavier <xmtrinidad@gmail.com>
2022-05-30 08:18:41 -0700

    added color preview tests

--- lit-testing/src/test/color-preview_test.ts
+++ lit-testing/src/test/color-preview_test.ts
@@ -1,7 +1,30 @@
-// import { ColorPreview } from "../color-preview";
+import { ColorPreview } from "../color-preview";
 
-// import {fixture, assert} from '@open-wc/testing';
+import {fixture, assert} from '@open-wc/testing';
 
-// suite('ColorPreview', () => {
+import {html} from 'lit/static-html.js';
 
-// });
+suite('color-preview', () => {
+
+  test('is defined', () => {
+    const el = document.createElement('color-preview');
+    assert.instanceOf(el, ColorPreview);
+  });
+
+  test('renders with default values', async () => {
+    const el = await fixture(html`<color-preview></color-preview>`);
+
+    assert.shadowDom.equal(
+      el,
+      `
+      <div class="color-preview">
+        <div class="input-container">
+          <label for="hex_input">Add Hex Code:</label>
+          <input type="text" id="hex_input">
+          <button>Submit</button>
+        </div>
+      </div>
+    `);
+  });
+
+});
